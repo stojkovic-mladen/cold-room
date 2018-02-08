9d2fd90673105f3103316ce1726de6750ce532ee
stojkovic-mladen <stojkovicmladen87@gmail.com>
2018-02-08 20:38:51 +0100

    dj pages

--- js/main.js
+++ js/main.js
@@ -1,21 +1,3 @@
 $(document).ready(function(){
-  $('.next').on('click', function(){
-    var currentImg = $('.active');
-    var nextImg = currentImg.next();
 
-    if(nextImg.length){
-      $(currentImg, this).removeClass('active').css('z-index', -10);
-      nextImg.addClass('active').css('z-index', 10);
-    }
-  });
-
-  $('.prev').on('click', function(){
-    var currentImg = $('.active');
-    var prevImg = currentImg.prev();
-
-    if(prevImg.length){
-      $(currentImg, this).removeClass('active').css('z-index', -10);
-      prevImg.addClass('active').css('z-index', 10);
-    }
-  });
 });
